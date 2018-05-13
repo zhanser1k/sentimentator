c7480e14cfbc6f3c4863bfa4f489eee4646a2da3
Zhanserik Baykadir <zhanserik.baikadir@gmail.com>
2018-05-14 04:56:34 +0700

    fixed python re expressions

--- cnn/data_helpers.py
+++ cnn/data_helpers.py
@@ -9,19 +9,11 @@ def clean_str(string):
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
-    string = re.sub(r"[^А-Яа-я0-9(),!?\'\`]", " ", string)
-    string = re.sub(r"\'s", " \'s", string)
-    string = re.sub(r"\'ve", " \'ve", string)
-    string = re.sub(r"n\'t", " n\'t", string)
-    string = re.sub(r"\'re", " \'re", string)
-    string = re.sub(r"\'d", " \'d", string)
-    string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r",", " , ", string)
-    string = re.sub(r"!", " ! ", string)
-    string = re.sub(r"\(", " \( ", string)
-    string = re.sub(r"\)", " \) ", string)
-    string = re.sub(r"\?", " \? ", string)
-    string = re.sub(r"\s{2,}", " ", string)
+    pat1 = r'@[А-Яа-я0-9]+'
+    pat2 = r'https?://[A-Za-z0-9./]+'
+    combined_pat = r'|'.join((pat1, pat2))
+    string = re.sub("[^а-яА-Я()]", " ", string)
+    string = re.sub(combined_pat, '', string)
     return string.strip().lower()
 
 
